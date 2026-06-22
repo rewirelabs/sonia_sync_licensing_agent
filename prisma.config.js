1ee6c66fc198dd609ecb6875281f2b566d42d407
Gianmarco Mezzina <gianmarco.mezzina@gmail.com>
2026-06-22 11:45:31 +0200

    fix: add seed command to prisma config

--- prisma.config.js
+++ prisma.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
+    seed: "npx tsx prisma/seed.ts",
   },
   datasource: {
     url: "file:./dev.db",
